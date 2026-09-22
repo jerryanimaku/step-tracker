f62b038fb51d6d07428fed67dcf9bfa1524966f0
Jeremiah Animaku <jeremiahanimaku@gmail.com>
2026-09-22 15:03:58 -0500

    created xcode project

---
+++ Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Jeremiah Animaku on 9/22/26.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
